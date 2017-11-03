3fdaac052c4cbdc8890e938ece1955672e19f4cf
saike1119 <saike1119@gmail.com>
2017-11-04 07:58:22 +0900

    インデント

--- WalkCount.php
+++ WalkCount.php
@@ -1,16 +1,17 @@
 <?php
-$start =2;
-$goal =100;
-$move =2;
-$count =0;
+$start = 2;
+$goal = 100;
+$move = 2;
+$count = 0;
 
-printf("ゴールまで歩いた回数は" . walk($start,$goal,$move,$count));
+printf("ゴールまで歩いた回数は" . walk($start, $goal, $move, $count));
 
-function walk($start, $goal, $move,$count)
+function walk($start, $goal, $move, $count)
 {
     while ($start < $goal) {
-      $start += $move;
-      $count++;
+        $start += $move;
+        $count++;
     }
     return $count;
 }
+
