034ac50c51c0ed7341ee986df451d0a974fa8b38
saike1119 <saike1119@gmail.com>
2017-11-04 16:24:39 +0900

    ファイル追加と削除

--- WalkCount.php
+++ WalkCount.php
@@ -1,17 +1,51 @@
 <?php
-$start = 2;
-$goal = 100;
-$move = 2;
-$count = 0;
 
-printf("ゴールまで歩いた回数は" . walk($start, $goal, $move, $count));
-
-function walk($start, $goal, $move, $count)
+class WalkCount
 {
-    while ($start < $goal) {
-        $start += $move;
-        $count++;
+    public $start = 10;
+    public $goal = 100;
+    public $move = 2;
+    public $count = 0;
+    public $name = 'さんの';
+
+    /**
+     * @param int $start
+     * @param int $goal
+     * @param int $move
+     * @param int $count
+     * @return array
+     */
+    static public function walk($start, $goal, $move, $count)
+    {
+        while ($start < $goal) {
+            $start += $move;
+            $count++;
+        }
+        $result = array(
+            "合計" => $count,
+            "歩数" => $start
+        );
+        return $result;
+
     }
-    return $count;
+
+    public function setName($name)
+    {
+        if (is_string($name)) {
+            $result = printf($name . $this->name);
+            return $result;
+        } else {
+            return "入ってないよ!";
+        }
+    }
+
 }
 
+$const = new WalkCount();
+$ww = $const->walk($const->start, $const->goal, $const->move, $const->count);
+echo $const->setName(1) . "歩いた合計は" . $ww{'合計'};var_dump('');
+echo $const->setName('') . "歩いた合計は" . $ww{'合計'};
+echo $const->setName('田中') . "歩いた歩数は" . $ww{'歩数'};
+foreach ($ww as $value){
+echo "valueは".$value;
+};
--- index.php
+++ index.php
@@ -4,14 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Vagrant Congratulations Success!!!!</title>
-    <style type="text/css" media="screen">
-        body{
-            background: #000;
-            color: #fff
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 <body>
-    <h1>Vagrant Congratulations Success!!!!</h1>
+<h1>Vagrant Congratulations Success!!!!</h1>
+<div class="box">
+    <p>Test!!</p>
+</div>
+<a href="#" target="_blank">OHAYO!</a>
 </body>
 </html>
